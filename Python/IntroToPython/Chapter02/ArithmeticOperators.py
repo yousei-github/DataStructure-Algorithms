6b16619e5bf7824622a122f405ce9ce12dbeac2a
Yousei <youseifs422@outlook.com>
2023-11-09 00:18:45 +0900

    Add Arithmetic Code in Python

---
+++ Python/IntroToPython/Chapter02/ArithmeticOperators.py
@@ -0,0 +1,23 @@
+# Addition (+)
+print('1 + 1 =', 1 + 1)
+
+# Subtraction (-)
+print('1 - 1 =', 1 - 1)
+
+# Multiplication (*)
+print('7 * 4 =', 7 * 4)
+
+# Exponentiation (**)
+print('2 ** 10 =', 2 ** 10)
+
+# True Division (/)
+print('7 / 4 =', 7 / 4, ', -13 / 4 =', -13 / 4)
+
+# Floor Division (//)
+print('7 // 4 =', 7 // 4, ', -13 // 4 =', -13 // 4)
+
+# To calculate the square root, you can use the exponent 1/2 (that is, 0.5):
+print('9 ** (1 / 2) =', 9 ** (1 / 2))
+
+# Remainder Operator (%)
+print('17 % 5 =', 17 % 5, ', 7.5 % 3.5 =', 7.5 % 3.5)
