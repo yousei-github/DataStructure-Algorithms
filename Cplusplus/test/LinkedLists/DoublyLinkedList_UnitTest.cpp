8ad570dab800f56f947c56ea663b3b30f252ec3e
Yousei <youseifs422@outlook.com>
2023-11-25 23:39:15 +0900

    Update Unit Test

--- Cplusplus/test/LinkedLists/DoublyLinkedList_UnitTest.cpp
+++ Cplusplus/test/LinkedLists/DoublyLinkedList_UnitTest.cpp
@@ -5,6 +5,7 @@
 /* Macro */
 
 /* Type */
+typedef int instantiationType;
 
 /* Prototype */
 
@@ -12,8 +13,40 @@
 
 /* Function */
 
-// Tests positive input.
-TEST(IsPrimeTest, Positive)
+TEST(DoublyLinkedList, Empty)
 {
-    EXPECT_FALSE(false);
+    /* Arrange */
+    LinkedLists::DoublyLinkedList<instantiationType> sut;
+
+    /* Act */
+    bool isEmpty = sut.empty();
+
+    /* Assert */
+    EXPECT_TRUE(isEmpty) << "The doubly linked list should be empty.";
+}
+
+TEST(DoublyLinkedList, AddOneInFront)
+{
+    /* Arrange */
+    LinkedLists::DoublyLinkedList<instantiationType> sut;
+    instantiationType element = 6;
+
+    /* Act */
+    sut.addFront(element);
+
+    /* Assert */
+    EXPECT_EQ(sut.front(), element) << "The element in the front node should be " << element;
+}
+
+TEST(DoublyLinkedList, AddOneInBack)
+{
+    /* Arrange */
+    LinkedLists::DoublyLinkedList<instantiationType> sut;
+    instantiationType element = 8;
+
+    /* Act */
+    sut.addBack(element);
+
+    /* Assert */
+    EXPECT_EQ(sut.back(), element) << "The element in the back node should be " << element;
 }
